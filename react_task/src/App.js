22b2bd32d205d55e78efa3f016b12a86fbaabe7a
MuskanMainuddin-code <404patelmuskan@gmail.com>
2023-03-25 20:30:56 +0530

    used react router and set paths for components

--- react_task/src/App.js
+++ react_task/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Manatee from './components/Manatee/Manatee';
+import Narwhal from './components/Narwhal/Narwhal';
+import Whale from './components/Whale/Whale';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Manatee/>} />
+        <Route path="manatee" element={<Manatee/>} />
+        <Route path="narwhal" element={<Narwhal/>} />
+        <Route path="whale" element={<Whale/>} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
